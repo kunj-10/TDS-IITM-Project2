66a4a013104bcac04a84f09c8c906c8f7d516f88
kunj_10 <kunjvgoyal30@gmail.com>
2024-12-15 14:07:58 +0530

    minor changes

--- autolysis.py
+++ autolysis.py
@@ -376,8 +376,8 @@ if __name__ == "__main__":
     df, cluster_centers, analysis_text_kmeans, kmeans_image = perform_kmeans_clustering(df,filename)
 
     # Narrations
-    # narration = get_narration(data_analysis, cluster_centers, corr, df.shape)
-    narration = ""
+    narration = get_narration(data_analysis, cluster_centers, corr, df.shape)
+    # narration = ""
 
     writeReadme(df, filename, data_analysis, corr, histogram_column, piechart_column, kmeans_image, narration)
 
